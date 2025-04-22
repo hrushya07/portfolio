638d246d62dd7ba507d50d4a08cc5e1bda3e467f
Hrushikesh Pardeshi <hrpardeshi77@gmail.com>
2025-04-22 11:48:37 +0530

    adds home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,20 +2,9 @@
 import { useState, useEffect } from "react";
 
 export default function Home() {
-    const [isLoading, setIsLoading] = useState(true);
-    const [isContentReady, setIsContentReady] = useState(false);
-
-    useEffect(() => {
-        if (!isLoading) {
-            setTimeout(() => {
-                setIsContentReady(true);
-            }, 100);
-        }
-    }, [isLoading]);
-
     return (
         <main className="min-h-screen bg-gray-200">
-           
+           Hello World
         </main>
     );
 }
